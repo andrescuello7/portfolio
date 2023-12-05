39415901562113a59237335b4a7114d7fee9e505
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-12-05 11:59:12 -0300

    [ADD] update profolio lengiajes and skils

--- src/components/Present/Present.js
+++ src/components/Present/Present.js
@@ -6,7 +6,7 @@ import "./Present.css"
 import UseCss from "../../useForm/useCss"
 
 //Img for present
-import perfil from "../../assets/icon_dev.png"
+import perfil from "../../img/perfil.jpg"
 
 const Present = () => {
     useEffect(() => {
@@ -19,24 +19,24 @@ const Present = () => {
     return (
         <div className="carrucel">
             <div className="CarrucelText">
-                <p className='mx-5 text-start aboutDescription'>
+                <p className='mx-2 text-start aboutDescription'>
                     Hello_
                 </p>
-                <div className='px-5'>
+                <div className='px-2'>
                     <div className='d-flex'>
                         <p>I'm </p> <p className="px-2 text-primary" id="myElement"></p>
                     </div>
                     <div>
-                        <p className='mx-0 text-start aboutDescription-2'> Passionate about web development. I spend my free time learning about technology and organization.</p>
+                        <p className='mx-0 text-start aboutDescription-2'>Passionate about web development. I spend my free time learning about technology and organization.</p>
                     </div>
                 </div>
-                <div className="social mt-5">
-                    <button className='buttonPdf' href="curriculumviate.pdf" download="curriculumviate.pdf">
+                <div className="social mt-5 mx-2">
+                    <a className='buttonPdf' href="curriculumviate.pdf" download="curriculumviate.pdf">
                         Curriculum
-                    </button>
-                    <button href="https://github.com/andrescuello7" className='mx-2 buttonPdf'>
+                    </a>
+                    <a href="https://github.com/andrescuello7" target='_' className='mx-2 buttonPdf'>
                         GitHub
-                    </button>
+                    </a>
                 </div>
             </div>
             <div className="CarrucelWidth">
